0cc1b3c7dffada10c7df8616478116a3cdfbb2ae
Alp Toker <tokeralp@gmail.com>
2020-11-22 10:33:58 +0300

    AssetsUpdate
    Assets path updated

--- application/views/containers/head.php
+++ application/views/containers/head.php
@@ -2,6 +2,6 @@
         <meta charset="utf-8" />
 
         <!-- CSS -->
-        <link rel="stylesheet" href="css/normalize.css" />
-        <link rel="stylesheet" href="css/style.css" />
+        <link rel="stylesheet" href="<?php echo ASSETS; ?>css/normalize.css" />
+        <link rel="stylesheet" href="<?php echo ASSETS; ?>css/style.css" />
         <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:400italic,700italic,400,700" />
