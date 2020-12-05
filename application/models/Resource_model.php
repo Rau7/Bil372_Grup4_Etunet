7a4bc9b90545f15ea74e057f7599ca3a6952408c
Adnan Ekici <53556022+AdnanEkici@users.noreply.github.com>
2020-12-05 22:02:49 +0300

    Create Resource_model.php

---
+++ application/models/Resource_model.php
@@ -0,0 +1,32 @@
+<?php
+class Resource_model extends CI_Model {
+
+    function getResources($data){
+
+        return $this->db->query("SELECT * FROM resource WHERE resource_course_id = ".$this->db->escape($data)." ")->result_array();
+    }
+
+    function getResource($resource_id){
+
+        return $this->db->query("SELECT * FROM resource WHERE resource_id = ".$resource_id." ")->row_array(0,"array");
+    }
+
+    function addResource($data,$course_id){
+
+        $this->db->query("INSERT INTO resource SET resource_short_desc=".$this->db->escape($data['resource_short_desc'])." , resource_long_desc=".$this->db->escape($data['resource_long_desc']).",  resource_url=".$this->db->escape($data['resource_url']).", resource_course_id='".$course_id."' ");
+
+    }
+
+    function updateResource($resoruce,$course_id,$resource_id){
+
+        $this->db->query("UPDATE resource SET resource_short_desc = ".$this->db->escape($resoruce['resource_short_desc']).", resource_long_desc = ".$this->db->escape($resoruce['resource_long_desc']).", resource_url=".$this->db->escape($resoruce['resource_url'])." WHERE resource_course_id = '".$course_id."' AND resource_id = '".$resource_id."'  ");
+    }
+
+    function deleteResource($resource_id){
+
+        $this->db->query("DELETE FROM resource WHERE resource_id = ".$resource_id." ");
+
+    }
+
+}
+?>
