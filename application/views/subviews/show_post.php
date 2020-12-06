c5d04ba9050a4acb95615e166cc206250d8b0689
Melissa Göşmen <47207804+melissagosmen@users.noreply.github.com>
2020-12-07 02:07:30 +0300

    show comment bölmesi eklendi

--- application/views/subviews/show_post.php
+++ application/views/subviews/show_post.php
@@ -14,4 +14,30 @@
 
 <hr class="rounded" style="width: 250%;">
 
-<h1>Comments will be added</h1>
+<h1>Comments</h1>
+<?php foreach ($comments as $comment) { ?>
+    <div class="card" style="width: 250%;">
+      <div class="card-body">
+        <?php if($comment['comment_added_id'] == $added_id || $type === 'teacher'){ ?>
+            <a href="<?php echo MAIN; ?>Posts/delete_comment/<?php echo $comment['comment_id']; ?>" role="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span>
+            </a>
+        <?php } ?>
+        <blockquote class="blockquote mb-0">
+          <p><?php echo $comment['comment_desc']; ?></p>
+          <p><?php echo $comment['comment_date']; ?></p>
+          <?php if($comment['comment_added_type'] === 'teacher'){ ?>
+            <footer class="blockquote-footer"> <cite title="Source Title"><?php echo $comment_added_name[0]['teacher_name']; ?></cite></footer>
+          <?php } ?>
+          <?php if($comment['comment_added_type'] === 'student'){ ?>
+            <footer class="blockquote-footer"> <cite title="Source Title"><?php echo $comment_added_name[0]['student_name']; ?></cite></footer>
+          <?php } ?>
+        </blockquote>
+
+        <?php if($comment['comment_added_id'] == $added_id || $type === 'teacher'){ ?>
+        <div style="text-align: right;">
+            <a >Edit Comment</a>  
+        </div>
+        <?php } ?>
+      </div>
+    </div>
+<?php } ?>
