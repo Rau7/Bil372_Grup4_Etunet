13a620ddcdcf8b83df7be70370c8910a643c3b4d
Melissa Göşmen <47207804+melissagosmen@users.noreply.github.com>
2020-12-07 14:49:41 +0300

    textarea düzeltilmiştir

--- application/views/subviews/update_comment.php
+++ application/views/subviews/update_comment.php
@@ -3,7 +3,7 @@
 <form action="<?php echo MAIN; ?>Posts/handle_comment_update/<?php echo $comment['comment_id']; ?>/<?php echo $comment['comment_post_id']; ?>" method="POST">
   <div class="form-group">
     <label for="comment_des">Comment Description</label>
-    <input type="text" class="form-control" id="comment_des" aria-describedby="emailHelp" placeholder="Comment Description" name="comment_des" value="<?php echo $comment['comment_desc']; ?>">
+    <textarea type="text" class="form-control" id="comment_des" aria-describedby="emailHelp" placeholder="Comment Description" name="comment_des" value="<?php echo $comment['comment_desc']; ?>"></textarea>
   </div>
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
